a8bf8fe084a33140b5221818304f9a0901188faa
Faa Diallo <faadiallop@gmail.com>
2024-03-14 15:26:33 -0700

    Commented functions #1

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,6 +4,13 @@ This program encrypts and decrypts text using the Caesar cipher.
 """
 
 def check_int(string, one_or_zero):
+    """ Parameters: string: String to check.
+                    one_or_zero: Boolean if string needs to be a 1 or 0.
+
+        Return: Boolean if string is an integer
+
+        This function returns True or False if the string is an int.
+    """
     try:
         int(string)
     except ValueError:
@@ -15,6 +22,13 @@ def check_int(string, one_or_zero):
     return True
 
 def user_input():
+    """ Parameters: None
+        Return: 3-tuple of the input text, shift for caesar cipher and
+        whether to encrypt or decrypt the text.
+
+        This function reads in input from the user and returns those
+        values.
+    """
     text = input("Please type in your text: ")
     shift = input("Please type in your shift: ")
     while not check_int(shift, False):
@@ -27,8 +41,13 @@ def user_input():
     return text, shift, encrypt
 
 def main():
+    """ Parameters: None
+        Return: None
+
+        This function runs the code for the program.
+    """
     text, shift, encrypt = user_input()
-    print(text, type(shift), shift, type(encrypt), encrypt)
+
 
 if __name__ == "__main__":
     try:
