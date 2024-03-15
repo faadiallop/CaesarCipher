10f1c3a32731d2420067c01486a1945c778b0937
Faa Diallo <faadiallop@gmail.com>
2024-03-15 13:31:35 -0700

    Fixes #7. Outputs a file if the user input a file.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -5,6 +5,7 @@ This program encrypts and decrypts text using the Caesar cipher.
 import os
 import argparse
 
+
 def process_args():
     """ Parameters: None
         Return: argparse.Namespace object with the arguments. 
@@ -41,9 +42,11 @@ def process_args():
     args = parser.parse_args()
     return args.text_or_file, args.shift, not args.decrypt
 
-def char_mapping(char, shift, encryption):
+def char_mapping(char, shift, run_encryption):
     """ Parameters: char: String of a single character.
                     shift: Integer of the amount to shift alphabet by.
+                    run_encryption: Boolean of whether to run encryption
+                    or not.
         Return: The corresponding encrypted or decrypted character.
         
         This function maps between characters for the caesar cipher.
@@ -52,7 +55,7 @@ def char_mapping(char, shift, encryption):
     if not char.isalpha():
         return char
     start_letter = "A" if char.isupper() else "a"
-    right_shift = 1 if encryption else -1
+    right_shift = 1 if run_encryption else -1
     return chr((ord(char) - ord(start_letter) + (shift * right_shift)) %
                 alphabet_size + ord(start_letter))
 
@@ -76,15 +79,23 @@ def decrypt(ciphertext, shift):
     """
     return "".join([char_mapping(char, shift, False) for char in ciphertext])
 
-def output(text, encryption):
-    """ Parameters: text: String of text to output.
-                    encrytion: Boolean of whether the program encrypted 
+def output(text, run_encryption, file):
+    """ Parameters: text: String that is either some text or a
+                    run_encryption: Boolean of whether the program 
+                    encrypted or not.
+                    file: Boolean of whether the text is from a file 
                     or not.
-        Return: None
+        Return: String to print to the screen.
 
         This functions returns the output of the users program call.
     """
-    return f"{'Ciphertext:' if encryption else 'Plaintext:'} {text}"
+    if file:
+        file_name = "encryption_text" if run_encryption else "decryption_text"
+        with open(file_name, "w+", encoding="utf-8") as f:
+            f.write(text)
+        return f"Output has been written to {file_name}!"
+    else:
+        return f"{'Ciphertext:' if run_encryption else 'Plaintext:'} {text}"
 
 def to_text(text_or_file):
     """ Parameters: text_or_file: String that is either some text or a
@@ -109,7 +120,7 @@ def main():
     text_or_file, shift, run_encrypt = process_args()
     text = to_text(text_or_file)
     output_text = encrypt(text, shift) if run_encrypt else decrypt(text, shift)
-    print(output(output_text, run_encrypt))
+    print(output(output_text, run_encrypt, os.path.exists(text_or_file)))
 
 if __name__ == "__main__":
     try:
