bdf7c614637dd539b0bfd0f59f3384ce5e3a8003
Faa Diallo <faadiallop@gmail.com>
2024-03-16 21:07:10 -0700

    Fixed some issues with the code. Refactored. Completed freq analysis. Fixes #8

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,10 +4,9 @@ This program encrypts and decrypts text using the Caesar cipher.
 """
 import os
 import heapq
-import string
 import argparse
 from functools import reduce
-from collections import namedtuple, Counter
+from collections import Counter
 from frequencies import letter_frequencies
 
 def process_args():
@@ -30,7 +29,8 @@ def process_args():
         raise exception
 
     parser.add_argument(
-        "shift",
+        "--shift",
+        default=-1,
         type=lambda s: int(s) if 0 <= int(s) < 26
                        else raise_(ValueError("Invalid shift value.")),
         help=("An integer of the amount to shift alphabet by. "
@@ -43,8 +43,37 @@ def process_args():
         help=("An boolean of whether to decrypt or not. "
               "This is set to False by default.")
     )
+    parser.add_argument(
+        "-f",
+        "--freq-analysis",
+        action="store_true",
+        help=("An boolean of whether to use frequency analysis or not. "
+              "This is set to False by default.")
+    )
+    parser.add_argument(
+        "--path",
+        default="",
+        help=("Path to the dictionary file. Used with --freq-analysis. ",
+              "Default is '/usr/share/dict/word'.")
+    )
     args = parser.parse_args()
-    return args.text_or_file, args.shift, not args.decrypt
+
+    if args.freq_analysis and not args.decrypt:
+        parser.error("--freq-analysis requires --decrypt(-d)")
+    if not args.freq_analysis and args.shift == -1:
+        parser.error("A shift value is required.")
+    if args.path and not os.path.isfile(args.path):
+        parser.error("This file does not exist.")
+    if args.path and not args.freq_analysis:
+        s = "A path is only available for the --freq-analysis(-f) option"
+        parser.error(s)
+    if args.freq_analysis and not args.path:
+        args.path = "/usr/share/dict/word"
+    return (args.text_or_file,
+            args.shift,
+            not args.decrypt,
+            args.freq_analysis,
+            args.path)
 
 def char_mapping(char, shift, run_encryption):
     """ Parameters: char: String of a single character.
@@ -84,6 +113,12 @@ def decrypt(ciphertext, shift):
     return "".join([char_mapping(char, shift, False) for char in ciphertext])
 
 def get_dictionary(path):
+    """ Parameters: path: String of path to a dictionary of words.
+        Return: Set of words from dictionary text file.
+
+        This function takes in a path to a dictionary text file and 
+        outputs a set of the words from that text file.
+    """
     words = set()
     with open(path, "r", encoding="utf-8") as file:
         for line in file:
@@ -93,6 +128,8 @@ def get_dictionary(path):
 def decrypt_freq_analysis(ciphertext, letter_freqs, dictionary):
     """ Parameters: ciphertext: String of ciphertext.
                     letter_freqs: Dictionary of letter frequencies. 
+                    dictionary: Set of words to compare plaintext 
+                    against.
         Return: NamedTuple of plaintext and shift.
 
         This function takes in a string and outputs the corresponding
@@ -100,7 +137,9 @@ def decrypt_freq_analysis(ciphertext, letter_freqs, dictionary):
     """
     letter_count = Counter([l for l in ciphertext.upper() if l.isalpha()])
     # Returns the most frequent letter from the cipher text
-    most_freq_text = reduce(lambda k1, k2: k1 if letter_count[k1] > letter_count[k2] else k2, letter_count)
+    most_freq_text = reduce(lambda k1, k2:
+                            k1 if letter_count[k1] > letter_count[k2] 
+                            else k2, letter_count)
     heap = [(-value, key) for key, value in letter_freqs.items()]
     heapq.heapify(heap)
     while True:
@@ -108,7 +147,6 @@ def decrypt_freq_analysis(ciphertext, letter_freqs, dictionary):
         shift = abs(ord(most_freq_text) - ord(most_freq))
         plaintext = decrypt(ciphertext, shift)
 
-        table = str.maketrans("", "", string.punctuation)
         plaintext_iter = plaintext.upper().split()
         count = 0
         for word in plaintext_iter:
@@ -116,10 +154,8 @@ def decrypt_freq_analysis(ciphertext, letter_freqs, dictionary):
                 count += 1
         if count / len(plaintext_iter) >= .5:
             break
-        
-    result_shift = namedtuple("result_shift", ["plaintext", "shift"])
-    result = result_shift(plaintext, shift)
-    return result
+
+    return f"Shift: {shift}\n{plaintext}"
 
 
 def output(text, run_encryption, file):
@@ -147,7 +183,7 @@ def to_text(text_or_file):
 
         This function outputs the text of a file or the string itself.
     """
-    if os.path.exists(text_or_file):
+    if os.path.isfile(text_or_file):
         with open(text_or_file, "r", encoding="utf-8") as file:
             text = file.read()
     else:
@@ -160,15 +196,20 @@ def main():
 
         This function runs the code for the program.
     """
-    path = "/usr/share/dict/words"
-    dictionary = get_dictionary(path)
-    result = decrypt_freq_analysis(open("input_to_decrypt", "r", encoding="utf-8").read(), letter_frequencies, dictionary)
-    print(result.plaintext, result.shift)
-    return
-    text_or_file, shift, run_encrypt = process_args()
+    text_or_file, shift, run_encrypt, freq_analysis, path = process_args()
+    if freq_analysis:
+        dictionary = get_dictionary(path)
     text = to_text(text_or_file)
-    output_text = encrypt(text, shift) if run_encrypt else decrypt(text, shift)
-    print(output(output_text, run_encrypt, os.path.exists(text_or_file)))
+    if not run_encrypt:
+        if freq_analysis:
+            output_text = decrypt_freq_analysis(text,
+                                                letter_frequencies,
+                                                dictionary)
+        else:
+            output_text = decrypt(text, shift)
+    else:
+        output_text = encrypt(text,shift)
+    print(output(output_text, run_encrypt, os.path.isfile(text_or_file)))
 
 if __name__ == "__main__":
     try:
